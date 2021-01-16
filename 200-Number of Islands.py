470605ea746e1f06cb015deb0dd56cf8195d5d22
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2021-01-15 17:25:15 -0800

    Update 200-Number of Islands.py
    Updated with DFS, recursive.

--- 200-Number of Islands.py
+++ 200-Number of Islands.py
@@ -35,32 +35,32 @@ grid = [
   ["0","0","0","1","1"]
 ]
 
-class Solution:
-    def numIslands(self, grid: list(list())) -> int:
-        res = 0
-        q, v = [], set() # The Set v is to keep track of the visited elements.
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if (grid[i][j] == "1") and ((i, j) not in v):
-                    q.append((i, j))
-                    v.add((i, j))
-                    for r, c in q: # For each "1" element, we need to check all 4 adjacent elements: up, down, left, and right directions.
-                        if c < len(grid[0])-1 and (grid[r][c+1] == "1") and ((r, c+1) not in v):
-                            q.append((r, c+1))
-                            v.add((r, c+1))
-                        if r < len(grid)-1 and (grid[r+1][c] == "1") and ((r+1, c) not in v):
-                            q.append((r+1, c))
-                            v.add((r+1, c))
-                        if r > 0 and (grid[r-1][c] == "1") and ((r-1, c) not in v):
-                            q.append((r-1, c))
-                            v.add((r-1, c))
-                        if c > 0 and (grid[r][c-1] == "1") and ((r, c-1) not in v):
-                            q.append((r, c-1))
-                            v.add((r, c-1))
-                    # print(q)
-                    q.clear()
-                    res += 1
-        return res
+# class Solution:
+#     def numIslands(self, grid: list(list())) -> int:
+#         res = 0
+#         q, v = [], set() # The Set v is to keep track of the visited elements.
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 if (grid[i][j] == "1") and ((i, j) not in v):
+#                     q.append((i, j))
+#                     v.add((i, j))
+#                     for r, c in q: # For each "1" element, we need to check all 4 adjacent elements: up, down, left, and right directions.
+#                         if c < len(grid[0])-1 and (grid[r][c+1] == "1") and ((r, c+1) not in v):
+#                             q.append((r, c+1))
+#                             v.add((r, c+1))
+#                         if r < len(grid)-1 and (grid[r+1][c] == "1") and ((r+1, c) not in v):
+#                             q.append((r+1, c))
+#                             v.add((r+1, c))
+#                         if r > 0 and (grid[r-1][c] == "1") and ((r-1, c) not in v):
+#                             q.append((r-1, c))
+#                             v.add((r-1, c))
+#                         if c > 0 and (grid[r][c-1] == "1") and ((r, c-1) not in v):
+#                             q.append((r, c-1))
+#                             v.add((r, c-1))
+#                     # print(q)
+#                     q.clear()
+#                     res += 1
+#         return res
 
 
 # Using while-loop
@@ -124,6 +124,30 @@ class Solution:
 
 # Runtime: 180 ms, faster than 15.95% of Python3 online submissions for Number of Islands.
 # Memory Usage: 19.5 MB, less than 10.87% of Python3 online submissions for Number of Islands.
+
+
+class Solution:
+    def numIslands(self, grid: list(list())) -> int:
+        def dfs(r, c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == "0" or v[r][c]:
+                return
+            v[r][c] = True
+            dfs(r, c+1)
+            dfs(r, c-1)
+            dfs(r+1, c)
+            dfs(r-1, c)
+        
+        res = 0
+        v = [[False for col in range(len(grid[0]))] for row in range(len(grid))]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (grid[i][j] == "1") and (not v[i][j]):
+                    dfs(i, j)
+                    res += 1
+        return res
+
+# Runtime: 144 ms, faster than 55.53% of Python3 online submissions for Number of Islands.
+# Memory Usage: 15.7 MB, less than 22.99% of Python3 online submissions for Number of Islands.
 
 solution = Solution()
 print(solution.numIslands(grid))
