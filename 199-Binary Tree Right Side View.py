0e62b02a2d5b57a9a5621444a70d0a778a924d29
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2021-02-06 16:50:23 -0800

    Update 199-Binary Tree Right Side View.py

--- 199-Binary Tree Right Side View.py
+++ 199-Binary Tree Right Side View.py
@@ -30,27 +30,53 @@ root.right.right = TreeNode(4)
 # Iteratively
 # BFS
 # Time complexity: O(n)
-import collections
-class Solution:
-    def rightSideView(self, root: TreeNode) -> list():
-        if not root: return []
-        queue = collections.deque([root])
-        res = []
-        while queue:
-            length = len(queue)
-            res.append(queue[0].val)
-            for _ in range(length):
-                curr = queue.popleft()
-                if curr.right:
-                    queue.append(curr.right)
-                if curr.left:
-                    queue.append(curr.left)
-
-        return res
+# import collections
+# class Solution:
+#     def rightSideView(self, root: TreeNode) -> list():
+#         if not root: return []
+#         queue = collections.deque([root])
+#         res = []
+#         while queue:
+#             length = len(queue)
+#             res.append(queue[0].val)
+#             for _ in range(length):
+#                 curr = queue.popleft()
+#                 if curr.right:
+#                     queue.append(curr.right)
+#                 if curr.left:
+#                     queue.append(curr.left)
+# 
+#         return res
 
 # Runtime: 24 ms, faster than 97.59% of Python3 online submissions for Binary Tree Right Side View.
 # Memory Usage: 14.4 MB, less than 21.59% of Python3 online submissions for Binary Tree Right Side View.
 
+
+# Recursively
+# DFS
+# Top-down
+# Pre-order
+# Time complexity: O(n)
+# First-in-first-serve, then update the result whenever there is a node to the right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> list():
+        self.res = []
+        def helper(node, depth):
+            if not node:
+                return None
+            if depth == len(self.res):
+                self.res.append(node.val)
+            else:
+                self.res[depth] = node.val
+            helper(node.left, depth+1)
+            helper(node.right, depth+1)
+            return node.val
+        
+        helper(root, 0)
+        return self.res
+
+# Runtime: 24 ms, faster than 97.61% of Python3 online submissions for Binary Tree Right Side View.
+# Memory Usage: 14.1 MB, less than 81.62% of Python3 online submissions for Binary Tree Right Side View.
 
 solution = Solution()
 print(solution.rightSideView(root))
