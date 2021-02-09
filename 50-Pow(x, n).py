4a0134b0b2818c11c71ad705055cda926389a6ee
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2021-02-09 10:59:13 -0800

    Update 50-Pow(x, n).py

--- 50-Pow(x, n).py
+++ 50-Pow(x, n).py
@@ -39,21 +39,47 @@ class Solution:
 
 # Refer to the post:
 # https://leetcode.com/problems/powx-n/discuss/182026/Python-or-Recursion-tm
+# class Solution:
+#     def myPow(self, x: float, n: int) -> float:
+#         if n == 0:
+#             return 1
+#         if n < 0:
+#             return 1 / self.myPow(x, -n)
+#         half = self.myPow(x, n//2)
+#         if n%2:
+#             return half * half * x
+#         else:
+#             return half * half
+
+# Runtime: 28 ms, faster than 83.08% of Python3 online submissions for Pow(x, n).
+# Memory Usage: 14 MB, less than 99.28% of Python3 online submissions for Pow(x, n).
+
+
+# 02/09/2021 working on Binary Search Explore Card
+# Refer to the post:
+# https://leetcode.com/problems/powx-n/discuss/345741/python3-Pow(xn)
+# x^n = (1/x)^(-n)
+# x^(2n) = (x^n) *(x^n)
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
         if n < 0:
-            return 1 / self.myPow(x, -n)
-        half = self.myPow(x, n//2)
-        if n%2:
-            return half * half * x
-        else:
-            return half * half
+            x = 1/x
+            n = -n
+        res = 1
+        tmp = x
+        while n > 0:
+            if n%2:
+                res *= tmp
+            tmp = tmp * tmp
+            n //= 2
+        return res
+
+# Runtime: 32 ms, faster than 64.34% of Python3 online submissions for Pow(x, n).
+# Memory Usage: 14.1 MB, less than 83.10% of Python3 online submissions for Pow(x, n).
+
 
 solution = Solution()
 print(solution.myPow(x, n))
 
-# Runtime: 28 ms, faster than 83.08% of Python3 online submissions for Pow(x, n).
-# Memory Usage: 14 MB, less than 99.28% of Python3 online submissions for Pow(x, n).
+
 
