29da0abb418842446f05a8b9c126f4c85c50346f
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2021-03-12 22:52:04 -0800

    Update 322-Coin Change.py

--- 322-Coin Change.py
+++ 322-Coin Change.py
@@ -24,37 +24,73 @@ Output: 2
 """
 
 # coins, amount = [1,2,5], 11 # 3
-# coins, amount = [2], 3 # -1
+coins, amount = [2], 3 # -1
 # coins, amount = [1], 0 # 0
 # coins, amount = [1], 1 # 1
 # coins, amount = [1], 2 # 2
 # coins, amount = [186,419,83,408], 6249 # 20
-coins, amount = [4,3,7,8], 9 # 3
+# coins, amount = [4,3,7,8], 9 # 3
 
 
 # Refer to post:
 # https://leetcode.com/problems/coin-change/discuss/77416/Python-11-line-280ms-DFS-with-early-termination-99-up
+
+
 from typing import List
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort(reverse = True)
-        lenc, self.res = len(coins), 2**31-1
-        
-        def dfs(pt, rem, count):
-            if not rem:
-                self.res = min(self.res, count)
-            for i in range(pt, lenc):
-                if coins[i] <= rem < coins[i] * (self.res-count): # if hope still exists
-                    dfs(i, rem-coins[i], count+1)
-    
-        for i in range(lenc):
-            dfs(i, amount, 0)
-        return self.res if self.res < 2**31-1 else -1
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         coins.sort(reverse = True)
+#         lenc, self.res = len(coins), 2**31-1
+#         
+#         def dfs(pt, rem, count):
+#             if not rem:
+#                 self.res = min(self.res, count)
+#             for i in range(pt, lenc):
+#                 if coins[i] <= rem < coins[i] * (self.res-count): # if hope still exists
+#                     dfs(i, rem-coins[i], count+1)
+#     
+#         for i in range(lenc):
+#             dfs(i, amount, 0)
+#         return self.res if self.res < 2**31-1 else -1
 
 
 
 # Runtime: 172 ms, faster than 97.36% of Python3 online submissions for Coin Change.
 # Memory Usage: 14.3 MB, less than 82.00% of Python3 online submissions for Coin Change.
+
+
+# My bruce force approach:
+# 32 / 182 test cases passed.
+# Status: Time Limit Exceeded
+# Last executed input:
+# [3,7,405,436]
+# 8839
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        def helper(sublist, target, track):
+            if not sublist: return
+            for i in range(len(sublist)):
+                q, r = divmod(target, sublist[i])
+                if r == 0:
+                    # print("r==0", sublist[i], q)
+                    self.output = min(self.output, track+q)
+                    # print("self.output: ", self.output)
+                    continue
+                c = 0
+                while c <= q:
+                    helper(sublist[i+1:], r, track+q-c)
+                    # print(sublist[i], q-c)
+                    c += 1
+                    r += sublist[i]
+        
+        if amount == 0:
+            return 0
+        self.output = float("inf")
+        count = 0
+        coins.sort(reverse=True)
+        helper(coins, amount, count)
+        return self.output if self.output != float("inf") else -1
+
 
 
 solution = Solution()
