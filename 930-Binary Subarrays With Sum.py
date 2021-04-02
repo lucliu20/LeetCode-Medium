1b5bffe73d031035ac07248d52eed97cf4cf8632
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2021-04-01 22:18:03 -0700

    Update 930-Binary Subarrays With Sum.py

--- 930-Binary Subarrays With Sum.py
+++ 930-Binary Subarrays With Sum.py
@@ -34,22 +34,49 @@ A, S = [1,0,1,0,1], 2
 # Then we are searching for how many times we see both (currSum == S) and (x) conditions when iterating the array.
 import collections
 from typing import List
-class Solution:
-    def numSubarraysWithSum(self, A: List[int], S: int) -> int:
-        counter = collections.Counter()
-        res, currSum = 0, 0
-        for i in A:
-            currSum += i
-            if currSum == S:
-                res += 1
-            if counter[currSum-S]:
-                res += counter[currSum-S]
-            counter[currSum] += 1
-        return res
+# class Solution:
+#     def numSubarraysWithSum(self, A: List[int], S: int) -> int:
+#         counter = collections.Counter()
+#         res, currSum = 0, 0
+#         for i in A:
+#             currSum += i
+#             if currSum == S:
+#                 res += 1
+#             if counter[currSum-S]:
+#                 res += counter[currSum-S]
+#             counter[currSum] += 1
+#         return res
 
 
 # Runtime: 296 ms, faster than 24.38% of Python3 online submissions for Binary Subarrays With Sum.
 # Memory Usage: 17.5 MB, less than 55.43% of Python3 online submissions for Binary Subarrays With Sum.
+
+
+# Refer to the post:
+# https://leetcode.com/problems/binary-subarrays-with-sum/discuss/531899/Python-Sliding-window-and-Pre-sum
+# Two-pointers
+# why we're setting count =0 if i==1?
+# It is because if i == 1, the sum is going to be changed for sure. So you will have to restart counting valid answers. 
+# If i==0, then you can just continue to count based on your original count value because it does not matter how many new zeros you have.
+class Solution:
+    def numSubarraysWithSum(self, A: List[int], S: int) -> int:
+        l = count = res = s = 0
+        for r, i in enumerate(A):
+            s += i
+            if i == 1:
+                count = 0
+            while l<= r and s >= S:
+                if s == S:
+                    count += 1
+                s -= A[l]
+                l += 1
+            res += count
+        return res
+
+
+# Runtime: 256 ms, faster than 74.67% of Python3 online submissions for Binary Subarrays With Sum.
+# Memory Usage: 15 MB, less than 91.43% of Python3 online submissions for Binary Subarrays With Sum.
+
 
 
 # 26 / 59 test cases passed.
