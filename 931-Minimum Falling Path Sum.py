e8f9b36a895f3aa08ad5e62977d95dafe69706bb
Gang Liu <lucliu@hotmail.com>
2021-04-25 23:16:18 -0700

    Create 931-Minimum Falling Path Sum.py

---
+++ 931-Minimum Falling Path Sum.py
@@ -0,0 +1,61 @@
+# https://leetcode.com/problems/minimum-falling-path-sum/
+
+
+"""
+Example 1:
+Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+Output: 13
+Explanation: There are two falling paths with a minimum sum underlined below:
+[[2,1,3],      [[2,1,3],
+ [6,5,4],       [6,5,4],
+ [7,8,9]]       [7,8,9]]
+
+Example 2:
+Input: matrix = [[-19,57],[-40,-5]]
+Output: -59
+Explanation: The falling path with a minimum sum is underlined below:
+[[-19,57],
+ [-40,-5]]
+
+Example 3:
+Input: matrix = [[-48]]
+Output: -48
+"""
+
+
+# matrix = [[2,1,3],[6,5,4],[7,8,9]]
+# matrix = [[-19,57],[-40,-5]]
+matrix = [[-48]]
+
+
+
+# DP iteratively in place
+from typing import List
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        for i in range(1, len(matrix)):
+            for j in range(len(matrix)):
+                if j-1 in range(len(matrix)) and j+1 in range(len(matrix)):
+                    matrix[i][j] = min(matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1]) + matrix[i][j]
+                elif j-1 not in range(len(matrix)):
+                    matrix[i][j] = min(matrix[i-1][j], matrix[i-1][j+1]) + matrix[i][j]
+                elif j+1 not in range(len(matrix)):
+                    matrix[i][j] = min(matrix[i-1][j-1], matrix[i-1][j]) + matrix[i][j]
+        return min(matrix[len(matrix)-1])
+
+
+# Runtime: 128 ms, faster than 36.59% of Python3 online submissions for Minimum Falling Path Sum.
+# Memory Usage: 15 MB, less than 72.06% of Python3 online submissions for Minimum Falling Path Sum.
+
+
+
+# DP recursively with memorization
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        pass
+
+
+
+solution = Solution()
+print(solution.minFallingPathSum(matrix))
+
