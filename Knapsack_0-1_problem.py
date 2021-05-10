c34840c6a0bee8782aa4e590a9f99f9d1e5af3fb
Gang Liu <lucliu@hotmail.com>
2021-05-10 09:18:02 -0700

    Update Knapsack_0-1_problem.py

--- Knapsack_0-1_problem.py
+++ Knapsack_0-1_problem.py
@@ -61,6 +61,19 @@ class Solution:
 # Bottom-up with Tabulation
 # Time complexity: O(N*C) where ‘N’ is the number of items and ‘C’ is the knapsack capacity
 # Space complexity: O(N∗C)
+# Illustration of the example profits = [1, 6, 10, 16], weights = [1, 2, 3, 5], capacity = 7
+# 			                    capacity -->
+# profit []	weight []	index	0	1	2	3	4	5	6	7
+#   -	        -	        0	0	0	0	0	0	0	0	0
+#   1	        1	        1	0	1	1	1	1	1	1	1
+#   6	        2	        2	0	1	6	7	7	7	7	7
+#   10	        3	        3	0	1	6	10	11	16	17	17
+#   16	        5	        4	0	1	6	10	11	16	17	22(6+16)
+# 
+# dp[len(profits)][capacity] = dp[4][7] = max(dp[4-1][7], dp[4-1][7-2]+profits[4-1]) = max(17, 6+16) = max(17, 22) = 22
+
+
+
 class Solution:
     def knapsack(self, profits: List[int], weights: List[int], capacity) -> int:
         dp = [[0 for _ in range(capacity + 1)] for _ in range(len(profits) + 1)]
@@ -74,6 +87,11 @@ class Solution:
                 if weights[i-1] > c:
                     dp[i][c] = dp[i-1][c]
                 else:
+                    # what c-weights[i-1] refers to: it refers to the left available weight if we take the object with weight equal to weights[i-1]
+                    # say we are at the capacity of 7, and weight is weights[4]=5. if we take the weight 5 object, then the left available weight is 7-5=2.
+                    # so the total profit when we take the weitht 5 is (profits[4]=16) + whatever the profit is at the capacity 2 without this object weight=5.
+                    # the computed profit at capacity 2 is maximized as we are aware of, and it is 6.
+                    # so the total profit when we take the weight 5 object is 6+16=22.
                     dp[i][c] = max(dp[i-1][c], dp[i-1][c-weights[i-1]]+profits[i-1])
         return dp[len(profits)][capacity]
     
