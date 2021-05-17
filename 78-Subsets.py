e36d84aaf2ae6dd7f148d25fd48d1a38b3aab388
Gang Liu <lucliu@hotmail.com>
2021-05-17 12:24:16 -0700

    Update 78-Subsets.py

--- 78-Subsets.py
+++ 78-Subsets.py
@@ -11,41 +11,61 @@ Output: [[],[0]]
 """
 
 
-# nums = [1,2,3]
-nums = [0]
+nums = [1,2,3,4]
+# nums = [0]
 
 
+# Backtracking
+# Recursively
 from typing import List
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def isValid():
-            return True
+        def isValid(j, lengh, candidate):
+            if lengh - len(candidate) <= len(nums) - j: # the number of left elements in nums is larger than what the candidate array is required
+                return True
+            return False
         
-        def placing():
-            pass
+        def placing(n):
+            candidate.append(n)
 
         def removing():
-            pass
+            candidate.pop()
         
-        def foundSolution():
-            return True
+        def foundSolution(length, candidate):
+            if len(candidate) == length:
+                return True
+            return False
         
-        def backtracking(arr):
-            if foundSolution:
-                self.res.append(arr)
+        def backtracking(i, length, candidate):
+            if foundSolution(length, candidate):
+                solution = candidate.copy()
+                self.res.append(solution)
                 return
-            for i in range(len(arr)):
-                if isValid():
-                    placing()
-                    backtracking()
+            for j in range(i, len(nums)):
+                if isValid(j, length, candidate):
+                    placing(nums[j])
+                    backtracking(j+1, length, candidate)
                     removing()
+                else:
+                    break
         
         self.res = []
-        backtracking(nums)
+        for l in range(0, len(nums)+1):
+            candidate = []
+            backtracking(0, l, candidate)
         return self.res
 
 
+# Runtime: 32 ms, faster than 77.62% of Python3 online submissions for Subsets.
+# Memory Usage: 14.2 MB, less than 98.84% of Python3 online submissions for Subsets.
 
+
+solution = Solution()
+print(solution.subsets(nums))
+
+
+
+# Use built-in function
 import itertools
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
@@ -60,6 +80,7 @@ class Solution:
 
 # Runtime: 32 ms, faster than 77.62% of Python3 online submissions for Subsets.
 # Memory Usage: 14.4 MB, less than 50.68% of Python3 online submissions for Subsets.
+
 
 solution = Solution()
 print(solution.subsets(nums))
