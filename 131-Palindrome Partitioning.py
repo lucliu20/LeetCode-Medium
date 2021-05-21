bb12fc97298831668ec0b7a9eba8b05233d4ca98
Gang Liu <lucliu@hotmail.com>
2021-05-21 16:24:44 -0700

    Create 131-Palindrome Partitioning.py

---
+++ 131-Palindrome Partitioning.py
@@ -0,0 +1,140 @@
+# https://leetcode.com/problems/palindrome-partitioning/
+
+"""
+Example 1:
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+
+Example 2:
+Input: s = "a"
+Output: [["a"]]
+"""
+
+
+# s = "aab" # [["a","a","b"],["aa","b"]]
+# s = "a" # [["a"]]
+# s = "aabbcc" # [["a","a","b","b","c","c"],["a","a","b","b","cc"],["a","a","bb","c","c"],["a","a","bb","cc"],["aa","b","b","c","c"],["aa","b","b","cc"],["aa","bb","c","c"],["aa","bb","cc"]]
+# s = "aabbaa"
+# s = "aba"
+# Test case: #24
+# s = "ababbbabbaba"
+# [["a","b","a","b","b","b","a","b","b","a","b","a"],["a","b","a","b","b","b","a","b","b","aba"],["a","b","a","b","b","b","a","b","bab","a"],["a","b","a","b","b","b","a","bb","a","b","a"],["a","b","a","b","b","b","a","bb","aba"],["a","b","a","b","b","b","abba","b","a"],["a","b","a","b","b","bab","b","a","b","a"],["a","b","a","b","b","bab","b","aba"],["a","b","a","b","b","bab","bab","a"],["a","b","a","b","b","babbab","a"],["a","b","a","b","bb","a","b","b","a","b","a"],["a","b","a","b","bb","a","b","b","aba"],["a","b","a","b","bb","a","b","bab","a"],["a","b","a","b","bb","a","bb","a","b","a"],["a","b","a","b","bb","a","bb","aba"],["a","b","a","b","bb","abba","b","a"],["a","b","a","b","bbabb","a","b","a"],["a","b","a","b","bbabb","aba"],["a","b","a","bb","b","a","b","b","a","b","a"],["a","b","a","bb","b","a","b","b","aba"],["a","b","a","bb","b","a","b","bab","a"],["a","b","a","bb","b","a","bb","a","b","a"],["a","b","a","bb","b","a","bb","aba"],["a","b","a","bb","b","abba","b","a"],["a","b","a","bb","bab","b","a","b","a"],["a","b","a","bb","bab","b","aba"],["a","b","a","bb","bab","bab","a"],["a","b","a","bb","babbab","a"],["a","b","a","bbb","a","b","b","a","b","a"],["a","b","a","bbb","a","b","b","aba"],["a","b","a","bbb","a","b","bab","a"],["a","b","a","bbb","a","bb","a","b","a"],["a","b","a","bbb","a","bb","aba"],["a","b","a","bbb","abba","b","a"],["a","b","abbba","b","b","a","b","a"],["a","b","abbba","b","b","aba"],["a","b","abbba","b","bab","a"],["a","b","abbba","bb","a","b","a"],["a","b","abbba","bb","aba"],["a","bab","b","b","a","b","b","a","b","a"],["a","bab","b","b","a","b","b","aba"],["a","bab","b","b","a","b","bab","a"],["a","bab","b","b","a","bb","a","b","a"],["a","bab","b","b","a","bb","aba"],["a","bab","b","b","abba","b","a"],["a","bab","b","bab","b","a","b","a"],["a","bab","b","bab","b","aba"],["a","bab","b","bab","bab","a"],["a","bab","b","babbab","a"],["a","bab","bb","a","b","b","a","b","a"],["a","bab","bb","a","b","b","aba"],["a","bab","bb","a","b","bab","a"],["a","bab","bb","a","bb","a","b","a"],["a","bab","bb","a","bb","aba"],["a","bab","bb","abba","b","a"],["a","bab","bbabb","a","b","a"],["a","bab","bbabb","aba"],["a","babbbab","b","a","b","a"],["a","babbbab","b","aba"],["a","babbbab","bab","a"],["aba","b","b","b","a","b","b","a","b","a"],["aba","b","b","b","a","b","b","aba"],["aba","b","b","b","a","b","bab","a"],["aba","b","b","b","a","bb","a","b","a"],["aba","b","b","b","a","bb","aba"],["aba","b","b","b","abba","b","a"],["aba","b","b","bab","b","a","b","a"],["aba","b","b","bab","b","aba"],["aba","b","b","bab","bab","a"],["aba","b","b","babbab","a"],["aba","b","bb","a","b","b","a","b","a"],["aba","b","bb","a","b","b","aba"],["aba","b","bb","a","b","bab","a"],["aba","b","bb","a","bb","a","b","a"],["aba","b","bb","a","bb","aba"],["aba","b","bb","abba","b","a"],["aba","b","bbabb","a","b","a"],["aba","b","bbabb","aba"],["aba","bb","b","a","b","b","a","b","a"],["aba","bb","b","a","b","b","aba"],["aba","bb","b","a","b","bab","a"],["aba","bb","b","a","bb","a","b","a"],["aba","bb","b","a","bb","aba"],["aba","bb","b","abba","b","a"],["aba","bb","bab","b","a","b","a"],["aba","bb","bab","b","aba"],["aba","bb","bab","bab","a"],["aba","bb","babbab","a"],["aba","bbb","a","b","b","a","b","a"],["aba","bbb","a","b","b","aba"],["aba","bbb","a","b","bab","a"],["aba","bbb","a","bb","a","b","a"],["aba","bbb","a","bb","aba"],["aba","bbb","abba","b","a"]]
+# [["a","b","a","b","b","b","a","b","b","a","b","a"],["a","b","a","b","b","b","a","b","b","aba"],["a","b","a","b","b","b","a","b","baba"],["a","b","a","b","b","b","a","b","bab","a"],["a","b","a","b","b","b","a","bb","a","b","a"],["a","b","a","b","b","b","a","bb","aba"],["a","b","a","b","b","b","abba","b","a"],["a","b","a","b","b","bab","b","a","b","a"],["a","b","a","b","b","bab","b","aba"],["a","b","a","b","b","bab","baba"],["a","b","a","b","b","bab","bab","a"],["a","b","a","b","b","babbab","a"],["a","b","a","b","bb","a","b","b","a","b","a"],["a","b","a","b","bb","a","b","b","aba"],["a","b","a","b","bb","a","b","baba"],["a","b","a","b","bb","a","b","bab","a"],["a","b","a","b","bb","a","bb","a","b","a"],["a","b","a","b","bb","a","bb","aba"],["a","b","a","b","bb","abba","b","a"],["a","b","a","b","bbabb","a","b","a"],["a","b","a","b","bbabb","aba"],["a","b","a","b","bbabba","b","a"],["a","b","a","bb","b","a","b","b","a","b","a"],["a","b","a","bb","b","a","b","b","aba"],["a","b","a","bb","b","a","b","baba"],["a","b","a","bb","b","a","b","bab","a"],["a","b","a","bb","b","a","bb","a","b","a"],["a","b","a","bb","b","a","bb","aba"],["a","b","a","bb","b","abba","b","a"],["a","b","a","bb","bab","b","a","b","a"],["a","b","a","bb","bab","b","aba"],["a","b","a","bb","bab","baba"],["a","b","a","bb","bab","bab","a"],["a","b","a","bb","babbab","a"],["a","b","a","bbb","a","b","b","a","b","a"],["a","b","a","bbb","a","b","b","aba"],["a","b","a","bbb","a","b","baba"],["a","b","a","bbb","a","b","bab","a"],["a","b","a","bbb","a","bb","a","b","a"],["a","b","a","bbb","a","bb","aba"],["a","b","a","bbb","abba","b","a"],["a","b","a","bbbabbab","a"],["a","b","abbbabbaba"],["a","b","abbba","b","b","a","b","a"],["a","b","abbba","b","b","aba"],["a","b","abbba","b","baba"],["a","b","abbba","b","bab","a"],["a","b","abbba","bb","a","b","a"],["a","b","abbba","bb","aba"],["a","bab","b","b","a","b","b","a","b","a"],["a","bab","b","b","a","b","b","aba"],["a","bab","b","b","a","b","baba"],["a","bab","b","b","a","b","bab","a"],["a","bab","b","b","a","bb","a","b","a"],["a","bab","b","b","a","bb","aba"],["a","bab","b","b","abba","b","a"],["a","bab","b","bab","b","a","b","a"],["a","bab","b","bab","b","aba"],["a","bab","b","bab","baba"],["a","bab","b","bab","bab","a"],["a","bab","b","babbab","a"],["a","bab","bb","a","b","b","a","b","a"],["a","bab","bb","a","b","b","aba"],["a","bab","bb","a","b","baba"],["a","bab","bb","a","b","bab","a"],["a","bab","bb","a","bb","a","b","a"],["a","bab","bb","a","bb","aba"],["a","bab","bb","abba","b","a"],["a","bab","bbabb","a","b","a"],["a","bab","bbabb","aba"],["a","bab","bbabba","b","a"],["a","babbbab","b","a","b","a"],["a","babbbab","b","aba"],["a","babbbab","baba"],["a","babbbab","bab","a"],["a","babbbabb","a","b","a"],["a","babbbabb","aba"],["aba","b","b","b","a","b","b","a","b","a"],["aba","b","b","b","a","b","b","aba"],["aba","b","b","b","a","b","baba"],["aba","b","b","b","a","b","bab","a"],["aba","b","b","b","a","bb","a","b","a"],["aba","b","b","b","a","bb","aba"],["aba","b","b","b","abba","b","a"],["aba","b","b","bab","b","a","b","a"],["aba","b","b","bab","b","aba"],["aba","b","b","bab","baba"],["aba","b","b","bab","bab","a"],["aba","b","b","babbab","a"],["aba","b","bb","a","b","b","a","b","a"],["aba","b","bb","a","b","b","aba"],["aba","b","bb","a","b","baba"],["aba","b","bb","a","b","bab","a"],["aba","b","bb","a","bb","a","b","a"],["aba","b","bb","a","bb","aba"],["aba","b","bb","abba","b","a"],["aba","b","bbabb","a","b","a"],["aba","b","bbabb","aba"],["aba","b","bbabba","b","a"],["aba","bb","b","a","b","b","a","b","a"],["aba","bb","b","a","b","b","aba"],["aba","bb","b","a","b","baba"],["aba","bb","b","a","b","bab","a"],["aba","bb","b","a","bb","a","b","a"],["aba","bb","b","a","bb","aba"],["aba","bb","b","abba","b","a"],["aba","bb","bab","b","a","b","a"],["aba","bb","bab","b","aba"],["aba","bb","bab","baba"],["aba","bb","bab","bab","a"],["aba","bb","babbab","a"],["aba","bbb","a","b","b","a","b","a"],["aba","bbb","a","b","b","aba"],["aba","bbb","a","b","baba"],["aba","bbb","a","b","bab","a"],["aba","bbb","a","bb","a","b","a"],["aba","bbb","a","bb","aba"],["aba","bbb","abba","b","a"],["aba","bbbabbab","a"],["abab","b","b","a","b","b","a","b","a"],["abab","b","b","a","b","b","aba"],["abab","b","b","a","b","baba"],["abab","b","b","a","b","bab","a"],["abab","b","b","a","bb","a","b","a"],["abab","b","b","a","bb","aba"],["abab","b","b","abba","b","a"],["abab","b","bab","b","a","b","a"],["abab","b","bab","b","aba"],["abab","b","bab","baba"],["abab","b","bab","bab","a"],["abab","b","babbab","a"],["abab","bb","a","b","b","a","b","a"],["abab","bb","a","b","b","aba"],["abab","bb","a","b","baba"],["abab","bb","a","b","bab","a"],["abab","bb","a","bb","a","b","a"],["abab","bb","a","bb","aba"],["abab","bb","abba","b","a"],["abab","bbabb","a","b","a"],["abab","bbabb","aba"],["abab","bbabba","b","a"],["ababbbabba","b","a"]]
+# s = "baba" # [["b","a","b","a"],["b","aba"],["bab","a"]]
+# Wrong answer: [["b","a","b","a"],["b","aba"],["baba"],["bab","a"]]
+
+# Test case: #26
+s = "abbab" # [["a","b","b","a","b"],["a","b","bab"],["a","bb","a","b"],["abba","b"]]
+# Wrong answer: [["a","b","b","a","b"],["a","bbab"],["a","b","bab"],["a","bb","a","b"],["abba","b"]]
+
+
+
+# DFS
+# Recursively
+# Accepted ugly
+# With all the hashed out code lines
+from typing import List
+import collections
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:        
+        def dfs(memo, chars):
+            if len(chars) == 1:
+                # memo[chars] = chars
+                # return memo[chars]
+                return [[chars]]
+            if memo[chars]:
+                return memo[chars]
+            candidate = []
+            myset = set()
+            for i in range(len(chars)-1):
+                print(chars[0:i+1], chars[i+1:])
+                # myset = set()
+                # candidate = []
+                # left, right = [], []
+                left = dfs(memo, chars[0:i+1])
+                right = dfs(memo, chars[i+1:])
+                # left.append(dfs(memo, chars[0:i+1]))
+                # right.append(dfs(memo, chars[i+1:]))
+                tmp = []
+                for l in left:
+                    # tmp = []
+                    for r in right:
+                        # tmp.append([l,r])
+                        tmp.extend(l)
+                        tmp.extend(r)
+                    # candidate.extend([tmp]) # [[['a', 'a'], 'b'], ['aa', 'b']]
+                    # candidate.extend(tmp) # ['a', 'b', 'a', 'b', 'aa', 'b']
+                    # candidate.append(tmp) # [[['a', 'a'], 'b'], ['aa', 'b']]
+                        if tuple(tmp) not in myset:
+                            candidate.append(tmp)
+                            myset.add(tuple(tmp))
+                        if len(tmp) == 3 or len(tmp) == 2:
+                            if tmp[0] == tmp[-1]:
+                                pal = "".join(tmp)
+                                if pal not in myset:
+                                    candidate.append([pal])
+                                    myset.add(pal)
+                        tmp = []
+                # if left == right:
+                    # candidate.extend([left+right])
+                # if chars[0:i+1] == chars[i+1:]:
+                #     candidate.append([chars])
+                # memo[chars].append(candidate)
+                memo[chars] = candidate
+            return candidate
+        
+        memo = collections.defaultdict(list)
+        res = []
+        res = dfs(memo, s)
+        return res
+
+
+# Runtime: 964 ms, faster than 5.04% of Python3 online submissions for Palindrome Partitioning.
+# Memory Usage: 34.2 MB, less than 7.52% of Python3 online submissions for Palindrome Partitioning.
+
+
+
+
+# Without all the hashed out code lines
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:        
+        def dfs(memo, chars):
+            if len(chars) == 1:
+                return [[chars]]
+            if memo[chars]:
+                return memo[chars]
+            candidate = []
+            myset = set()
+            for i in range(len(chars)-1):
+                left = dfs(memo, chars[0:i+1])
+                right = dfs(memo, chars[i+1:])
+                tmp = []
+                for l in left:
+                    for r in right:
+                        tmp.extend(l)
+                        tmp.extend(r)
+                        if tuple(tmp) not in myset:
+                            candidate.append(tmp)
+                            myset.add(tuple(tmp))
+                        if len(tmp) == 3 or len(tmp) == 2:
+                            if tmp[0] == tmp[-1]:
+                                pal = "".join(tmp)
+                                if pal not in myset:
+                                    candidate.append([pal])
+                                    myset.add(pal)
+                        tmp = []
+                memo[chars] = candidate
+            return candidate
+        
+        memo = collections.defaultdict(list)
+        res = []
+        res = dfs(memo, s)
+        return res
+
+# Runtime: 972 ms, faster than 5.04% of Python3 online submissions for Palindrome Partitioning.
+# Memory Usage: 34.3 MB, less than 7.20% of Python3 online submissions for Palindrome Partitioning.
+
+
+solution = Solution()
+print(solution.partition(s))
+
