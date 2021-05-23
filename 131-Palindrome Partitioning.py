a1bdc527f89bf279b28909218e4c80397810e533
Gang Liu <lucliu@hotmail.com>
2021-05-22 19:25:35 -0700

    Update 131-Palindrome Partitioning.py

--- 131-Palindrome Partitioning.py
+++ 131-Palindrome Partitioning.py
@@ -18,8 +18,8 @@ Output: [["a"]]
 # s = "aba"
 # Test case: #24
 # s = "ababbbabbaba"
-# [["a","b","a","b","b","b","a","b","b","a","b","a"],["a","b","a","b","b","b","a","b","b","aba"],["a","b","a","b","b","b","a","b","bab","a"],["a","b","a","b","b","b","a","bb","a","b","a"],["a","b","a","b","b","b","a","bb","aba"],["a","b","a","b","b","b","abba","b","a"],["a","b","a","b","b","bab","b","a","b","a"],["a","b","a","b","b","bab","b","aba"],["a","b","a","b","b","bab","bab","a"],["a","b","a","b","b","babbab","a"],["a","b","a","b","bb","a","b","b","a","b","a"],["a","b","a","b","bb","a","b","b","aba"],["a","b","a","b","bb","a","b","bab","a"],["a","b","a","b","bb","a","bb","a","b","a"],["a","b","a","b","bb","a","bb","aba"],["a","b","a","b","bb","abba","b","a"],["a","b","a","b","bbabb","a","b","a"],["a","b","a","b","bbabb","aba"],["a","b","a","bb","b","a","b","b","a","b","a"],["a","b","a","bb","b","a","b","b","aba"],["a","b","a","bb","b","a","b","bab","a"],["a","b","a","bb","b","a","bb","a","b","a"],["a","b","a","bb","b","a","bb","aba"],["a","b","a","bb","b","abba","b","a"],["a","b","a","bb","bab","b","a","b","a"],["a","b","a","bb","bab","b","aba"],["a","b","a","bb","bab","bab","a"],["a","b","a","bb","babbab","a"],["a","b","a","bbb","a","b","b","a","b","a"],["a","b","a","bbb","a","b","b","aba"],["a","b","a","bbb","a","b","bab","a"],["a","b","a","bbb","a","bb","a","b","a"],["a","b","a","bbb","a","bb","aba"],["a","b","a","bbb","abba","b","a"],["a","b","abbba","b","b","a","b","a"],["a","b","abbba","b","b","aba"],["a","b","abbba","b","bab","a"],["a","b","abbba","bb","a","b","a"],["a","b","abbba","bb","aba"],["a","bab","b","b","a","b","b","a","b","a"],["a","bab","b","b","a","b","b","aba"],["a","bab","b","b","a","b","bab","a"],["a","bab","b","b","a","bb","a","b","a"],["a","bab","b","b","a","bb","aba"],["a","bab","b","b","abba","b","a"],["a","bab","b","bab","b","a","b","a"],["a","bab","b","bab","b","aba"],["a","bab","b","bab","bab","a"],["a","bab","b","babbab","a"],["a","bab","bb","a","b","b","a","b","a"],["a","bab","bb","a","b","b","aba"],["a","bab","bb","a","b","bab","a"],["a","bab","bb","a","bb","a","b","a"],["a","bab","bb","a","bb","aba"],["a","bab","bb","abba","b","a"],["a","bab","bbabb","a","b","a"],["a","bab","bbabb","aba"],["a","babbbab","b","a","b","a"],["a","babbbab","b","aba"],["a","babbbab","bab","a"],["aba","b","b","b","a","b","b","a","b","a"],["aba","b","b","b","a","b","b","aba"],["aba","b","b","b","a","b","bab","a"],["aba","b","b","b","a","bb","a","b","a"],["aba","b","b","b","a","bb","aba"],["aba","b","b","b","abba","b","a"],["aba","b","b","bab","b","a","b","a"],["aba","b","b","bab","b","aba"],["aba","b","b","bab","bab","a"],["aba","b","b","babbab","a"],["aba","b","bb","a","b","b","a","b","a"],["aba","b","bb","a","b","b","aba"],["aba","b","bb","a","b","bab","a"],["aba","b","bb","a","bb","a","b","a"],["aba","b","bb","a","bb","aba"],["aba","b","bb","abba","b","a"],["aba","b","bbabb","a","b","a"],["aba","b","bbabb","aba"],["aba","bb","b","a","b","b","a","b","a"],["aba","bb","b","a","b","b","aba"],["aba","bb","b","a","b","bab","a"],["aba","bb","b","a","bb","a","b","a"],["aba","bb","b","a","bb","aba"],["aba","bb","b","abba","b","a"],["aba","bb","bab","b","a","b","a"],["aba","bb","bab","b","aba"],["aba","bb","bab","bab","a"],["aba","bb","babbab","a"],["aba","bbb","a","b","b","a","b","a"],["aba","bbb","a","b","b","aba"],["aba","bbb","a","b","bab","a"],["aba","bbb","a","bb","a","b","a"],["aba","bbb","a","bb","aba"],["aba","bbb","abba","b","a"]]
-# [["a","b","a","b","b","b","a","b","b","a","b","a"],["a","b","a","b","b","b","a","b","b","aba"],["a","b","a","b","b","b","a","b","baba"],["a","b","a","b","b","b","a","b","bab","a"],["a","b","a","b","b","b","a","bb","a","b","a"],["a","b","a","b","b","b","a","bb","aba"],["a","b","a","b","b","b","abba","b","a"],["a","b","a","b","b","bab","b","a","b","a"],["a","b","a","b","b","bab","b","aba"],["a","b","a","b","b","bab","baba"],["a","b","a","b","b","bab","bab","a"],["a","b","a","b","b","babbab","a"],["a","b","a","b","bb","a","b","b","a","b","a"],["a","b","a","b","bb","a","b","b","aba"],["a","b","a","b","bb","a","b","baba"],["a","b","a","b","bb","a","b","bab","a"],["a","b","a","b","bb","a","bb","a","b","a"],["a","b","a","b","bb","a","bb","aba"],["a","b","a","b","bb","abba","b","a"],["a","b","a","b","bbabb","a","b","a"],["a","b","a","b","bbabb","aba"],["a","b","a","b","bbabba","b","a"],["a","b","a","bb","b","a","b","b","a","b","a"],["a","b","a","bb","b","a","b","b","aba"],["a","b","a","bb","b","a","b","baba"],["a","b","a","bb","b","a","b","bab","a"],["a","b","a","bb","b","a","bb","a","b","a"],["a","b","a","bb","b","a","bb","aba"],["a","b","a","bb","b","abba","b","a"],["a","b","a","bb","bab","b","a","b","a"],["a","b","a","bb","bab","b","aba"],["a","b","a","bb","bab","baba"],["a","b","a","bb","bab","bab","a"],["a","b","a","bb","babbab","a"],["a","b","a","bbb","a","b","b","a","b","a"],["a","b","a","bbb","a","b","b","aba"],["a","b","a","bbb","a","b","baba"],["a","b","a","bbb","a","b","bab","a"],["a","b","a","bbb","a","bb","a","b","a"],["a","b","a","bbb","a","bb","aba"],["a","b","a","bbb","abba","b","a"],["a","b","a","bbbabbab","a"],["a","b","abbbabbaba"],["a","b","abbba","b","b","a","b","a"],["a","b","abbba","b","b","aba"],["a","b","abbba","b","baba"],["a","b","abbba","b","bab","a"],["a","b","abbba","bb","a","b","a"],["a","b","abbba","bb","aba"],["a","bab","b","b","a","b","b","a","b","a"],["a","bab","b","b","a","b","b","aba"],["a","bab","b","b","a","b","baba"],["a","bab","b","b","a","b","bab","a"],["a","bab","b","b","a","bb","a","b","a"],["a","bab","b","b","a","bb","aba"],["a","bab","b","b","abba","b","a"],["a","bab","b","bab","b","a","b","a"],["a","bab","b","bab","b","aba"],["a","bab","b","bab","baba"],["a","bab","b","bab","bab","a"],["a","bab","b","babbab","a"],["a","bab","bb","a","b","b","a","b","a"],["a","bab","bb","a","b","b","aba"],["a","bab","bb","a","b","baba"],["a","bab","bb","a","b","bab","a"],["a","bab","bb","a","bb","a","b","a"],["a","bab","bb","a","bb","aba"],["a","bab","bb","abba","b","a"],["a","bab","bbabb","a","b","a"],["a","bab","bbabb","aba"],["a","bab","bbabba","b","a"],["a","babbbab","b","a","b","a"],["a","babbbab","b","aba"],["a","babbbab","baba"],["a","babbbab","bab","a"],["a","babbbabb","a","b","a"],["a","babbbabb","aba"],["aba","b","b","b","a","b","b","a","b","a"],["aba","b","b","b","a","b","b","aba"],["aba","b","b","b","a","b","baba"],["aba","b","b","b","a","b","bab","a"],["aba","b","b","b","a","bb","a","b","a"],["aba","b","b","b","a","bb","aba"],["aba","b","b","b","abba","b","a"],["aba","b","b","bab","b","a","b","a"],["aba","b","b","bab","b","aba"],["aba","b","b","bab","baba"],["aba","b","b","bab","bab","a"],["aba","b","b","babbab","a"],["aba","b","bb","a","b","b","a","b","a"],["aba","b","bb","a","b","b","aba"],["aba","b","bb","a","b","baba"],["aba","b","bb","a","b","bab","a"],["aba","b","bb","a","bb","a","b","a"],["aba","b","bb","a","bb","aba"],["aba","b","bb","abba","b","a"],["aba","b","bbabb","a","b","a"],["aba","b","bbabb","aba"],["aba","b","bbabba","b","a"],["aba","bb","b","a","b","b","a","b","a"],["aba","bb","b","a","b","b","aba"],["aba","bb","b","a","b","baba"],["aba","bb","b","a","b","bab","a"],["aba","bb","b","a","bb","a","b","a"],["aba","bb","b","a","bb","aba"],["aba","bb","b","abba","b","a"],["aba","bb","bab","b","a","b","a"],["aba","bb","bab","b","aba"],["aba","bb","bab","baba"],["aba","bb","bab","bab","a"],["aba","bb","babbab","a"],["aba","bbb","a","b","b","a","b","a"],["aba","bbb","a","b","b","aba"],["aba","bbb","a","b","baba"],["aba","bbb","a","b","bab","a"],["aba","bbb","a","bb","a","b","a"],["aba","bbb","a","bb","aba"],["aba","bbb","abba","b","a"],["aba","bbbabbab","a"],["abab","b","b","a","b","b","a","b","a"],["abab","b","b","a","b","b","aba"],["abab","b","b","a","b","baba"],["abab","b","b","a","b","bab","a"],["abab","b","b","a","bb","a","b","a"],["abab","b","b","a","bb","aba"],["abab","b","b","abba","b","a"],["abab","b","bab","b","a","b","a"],["abab","b","bab","b","aba"],["abab","b","bab","baba"],["abab","b","bab","bab","a"],["abab","b","babbab","a"],["abab","bb","a","b","b","a","b","a"],["abab","bb","a","b","b","aba"],["abab","bb","a","b","baba"],["abab","bb","a","b","bab","a"],["abab","bb","a","bb","a","b","a"],["abab","bb","a","bb","aba"],["abab","bb","abba","b","a"],["abab","bbabb","a","b","a"],["abab","bbabb","aba"],["abab","bbabba","b","a"],["ababbbabba","b","a"]]
+# Expected: [["a","b","a","b","b","b","a","b","b","a","b","a"],["a","b","a","b","b","b","a","b","b","aba"],["a","b","a","b","b","b","a","b","bab","a"],["a","b","a","b","b","b","a","bb","a","b","a"],["a","b","a","b","b","b","a","bb","aba"],["a","b","a","b","b","b","abba","b","a"],["a","b","a","b","b","bab","b","a","b","a"],["a","b","a","b","b","bab","b","aba"],["a","b","a","b","b","bab","bab","a"],["a","b","a","b","b","babbab","a"],["a","b","a","b","bb","a","b","b","a","b","a"],["a","b","a","b","bb","a","b","b","aba"],["a","b","a","b","bb","a","b","bab","a"],["a","b","a","b","bb","a","bb","a","b","a"],["a","b","a","b","bb","a","bb","aba"],["a","b","a","b","bb","abba","b","a"],["a","b","a","b","bbabb","a","b","a"],["a","b","a","b","bbabb","aba"],["a","b","a","bb","b","a","b","b","a","b","a"],["a","b","a","bb","b","a","b","b","aba"],["a","b","a","bb","b","a","b","bab","a"],["a","b","a","bb","b","a","bb","a","b","a"],["a","b","a","bb","b","a","bb","aba"],["a","b","a","bb","b","abba","b","a"],["a","b","a","bb","bab","b","a","b","a"],["a","b","a","bb","bab","b","aba"],["a","b","a","bb","bab","bab","a"],["a","b","a","bb","babbab","a"],["a","b","a","bbb","a","b","b","a","b","a"],["a","b","a","bbb","a","b","b","aba"],["a","b","a","bbb","a","b","bab","a"],["a","b","a","bbb","a","bb","a","b","a"],["a","b","a","bbb","a","bb","aba"],["a","b","a","bbb","abba","b","a"],["a","b","abbba","b","b","a","b","a"],["a","b","abbba","b","b","aba"],["a","b","abbba","b","bab","a"],["a","b","abbba","bb","a","b","a"],["a","b","abbba","bb","aba"],["a","bab","b","b","a","b","b","a","b","a"],["a","bab","b","b","a","b","b","aba"],["a","bab","b","b","a","b","bab","a"],["a","bab","b","b","a","bb","a","b","a"],["a","bab","b","b","a","bb","aba"],["a","bab","b","b","abba","b","a"],["a","bab","b","bab","b","a","b","a"],["a","bab","b","bab","b","aba"],["a","bab","b","bab","bab","a"],["a","bab","b","babbab","a"],["a","bab","bb","a","b","b","a","b","a"],["a","bab","bb","a","b","b","aba"],["a","bab","bb","a","b","bab","a"],["a","bab","bb","a","bb","a","b","a"],["a","bab","bb","a","bb","aba"],["a","bab","bb","abba","b","a"],["a","bab","bbabb","a","b","a"],["a","bab","bbabb","aba"],["a","babbbab","b","a","b","a"],["a","babbbab","b","aba"],["a","babbbab","bab","a"],["aba","b","b","b","a","b","b","a","b","a"],["aba","b","b","b","a","b","b","aba"],["aba","b","b","b","a","b","bab","a"],["aba","b","b","b","a","bb","a","b","a"],["aba","b","b","b","a","bb","aba"],["aba","b","b","b","abba","b","a"],["aba","b","b","bab","b","a","b","a"],["aba","b","b","bab","b","aba"],["aba","b","b","bab","bab","a"],["aba","b","b","babbab","a"],["aba","b","bb","a","b","b","a","b","a"],["aba","b","bb","a","b","b","aba"],["aba","b","bb","a","b","bab","a"],["aba","b","bb","a","bb","a","b","a"],["aba","b","bb","a","bb","aba"],["aba","b","bb","abba","b","a"],["aba","b","bbabb","a","b","a"],["aba","b","bbabb","aba"],["aba","bb","b","a","b","b","a","b","a"],["aba","bb","b","a","b","b","aba"],["aba","bb","b","a","b","bab","a"],["aba","bb","b","a","bb","a","b","a"],["aba","bb","b","a","bb","aba"],["aba","bb","b","abba","b","a"],["aba","bb","bab","b","a","b","a"],["aba","bb","bab","b","aba"],["aba","bb","bab","bab","a"],["aba","bb","babbab","a"],["aba","bbb","a","b","b","a","b","a"],["aba","bbb","a","b","b","aba"],["aba","bbb","a","b","bab","a"],["aba","bbb","a","bb","a","b","a"],["aba","bbb","a","bb","aba"],["aba","bbb","abba","b","a"]]
+# Wrong answer: # [["a","b","a","b","b","b","a","b","b","a","b","a"],["a","b","a","b","b","b","a","b","b","aba"],["a","b","a","b","b","b","a","b","baba"],["a","b","a","b","b","b","a","b","bab","a"],["a","b","a","b","b","b","a","bb","a","b","a"],["a","b","a","b","b","b","a","bb","aba"],["a","b","a","b","b","b","abba","b","a"],["a","b","a","b","b","bab","b","a","b","a"],["a","b","a","b","b","bab","b","aba"],["a","b","a","b","b","bab","baba"],["a","b","a","b","b","bab","bab","a"],["a","b","a","b","b","babbab","a"],["a","b","a","b","bb","a","b","b","a","b","a"],["a","b","a","b","bb","a","b","b","aba"],["a","b","a","b","bb","a","b","baba"],["a","b","a","b","bb","a","b","bab","a"],["a","b","a","b","bb","a","bb","a","b","a"],["a","b","a","b","bb","a","bb","aba"],["a","b","a","b","bb","abba","b","a"],["a","b","a","b","bbabb","a","b","a"],["a","b","a","b","bbabb","aba"],["a","b","a","b","bbabba","b","a"],["a","b","a","bb","b","a","b","b","a","b","a"],["a","b","a","bb","b","a","b","b","aba"],["a","b","a","bb","b","a","b","baba"],["a","b","a","bb","b","a","b","bab","a"],["a","b","a","bb","b","a","bb","a","b","a"],["a","b","a","bb","b","a","bb","aba"],["a","b","a","bb","b","abba","b","a"],["a","b","a","bb","bab","b","a","b","a"],["a","b","a","bb","bab","b","aba"],["a","b","a","bb","bab","baba"],["a","b","a","bb","bab","bab","a"],["a","b","a","bb","babbab","a"],["a","b","a","bbb","a","b","b","a","b","a"],["a","b","a","bbb","a","b","b","aba"],["a","b","a","bbb","a","b","baba"],["a","b","a","bbb","a","b","bab","a"],["a","b","a","bbb","a","bb","a","b","a"],["a","b","a","bbb","a","bb","aba"],["a","b","a","bbb","abba","b","a"],["a","b","a","bbbabbab","a"],["a","b","abbbabbaba"],["a","b","abbba","b","b","a","b","a"],["a","b","abbba","b","b","aba"],["a","b","abbba","b","baba"],["a","b","abbba","b","bab","a"],["a","b","abbba","bb","a","b","a"],["a","b","abbba","bb","aba"],["a","bab","b","b","a","b","b","a","b","a"],["a","bab","b","b","a","b","b","aba"],["a","bab","b","b","a","b","baba"],["a","bab","b","b","a","b","bab","a"],["a","bab","b","b","a","bb","a","b","a"],["a","bab","b","b","a","bb","aba"],["a","bab","b","b","abba","b","a"],["a","bab","b","bab","b","a","b","a"],["a","bab","b","bab","b","aba"],["a","bab","b","bab","baba"],["a","bab","b","bab","bab","a"],["a","bab","b","babbab","a"],["a","bab","bb","a","b","b","a","b","a"],["a","bab","bb","a","b","b","aba"],["a","bab","bb","a","b","baba"],["a","bab","bb","a","b","bab","a"],["a","bab","bb","a","bb","a","b","a"],["a","bab","bb","a","bb","aba"],["a","bab","bb","abba","b","a"],["a","bab","bbabb","a","b","a"],["a","bab","bbabb","aba"],["a","bab","bbabba","b","a"],["a","babbbab","b","a","b","a"],["a","babbbab","b","aba"],["a","babbbab","baba"],["a","babbbab","bab","a"],["a","babbbabb","a","b","a"],["a","babbbabb","aba"],["aba","b","b","b","a","b","b","a","b","a"],["aba","b","b","b","a","b","b","aba"],["aba","b","b","b","a","b","baba"],["aba","b","b","b","a","b","bab","a"],["aba","b","b","b","a","bb","a","b","a"],["aba","b","b","b","a","bb","aba"],["aba","b","b","b","abba","b","a"],["aba","b","b","bab","b","a","b","a"],["aba","b","b","bab","b","aba"],["aba","b","b","bab","baba"],["aba","b","b","bab","bab","a"],["aba","b","b","babbab","a"],["aba","b","bb","a","b","b","a","b","a"],["aba","b","bb","a","b","b","aba"],["aba","b","bb","a","b","baba"],["aba","b","bb","a","b","bab","a"],["aba","b","bb","a","bb","a","b","a"],["aba","b","bb","a","bb","aba"],["aba","b","bb","abba","b","a"],["aba","b","bbabb","a","b","a"],["aba","b","bbabb","aba"],["aba","b","bbabba","b","a"],["aba","bb","b","a","b","b","a","b","a"],["aba","bb","b","a","b","b","aba"],["aba","bb","b","a","b","baba"],["aba","bb","b","a","b","bab","a"],["aba","bb","b","a","bb","a","b","a"],["aba","bb","b","a","bb","aba"],["aba","bb","b","abba","b","a"],["aba","bb","bab","b","a","b","a"],["aba","bb","bab","b","aba"],["aba","bb","bab","baba"],["aba","bb","bab","bab","a"],["aba","bb","babbab","a"],["aba","bbb","a","b","b","a","b","a"],["aba","bbb","a","b","b","aba"],["aba","bbb","a","b","baba"],["aba","bbb","a","b","bab","a"],["aba","bbb","a","bb","a","b","a"],["aba","bbb","a","bb","aba"],["aba","bbb","abba","b","a"],["aba","bbbabbab","a"],["abab","b","b","a","b","b","a","b","a"],["abab","b","b","a","b","b","aba"],["abab","b","b","a","b","baba"],["abab","b","b","a","b","bab","a"],["abab","b","b","a","bb","a","b","a"],["abab","b","b","a","bb","aba"],["abab","b","b","abba","b","a"],["abab","b","bab","b","a","b","a"],["abab","b","bab","b","aba"],["abab","b","bab","baba"],["abab","b","bab","bab","a"],["abab","b","babbab","a"],["abab","bb","a","b","b","a","b","a"],["abab","bb","a","b","b","aba"],["abab","bb","a","b","baba"],["abab","bb","a","b","bab","a"],["abab","bb","a","bb","a","b","a"],["abab","bb","a","bb","aba"],["abab","bb","abba","b","a"],["abab","bbabb","a","b","a"],["abab","bbabb","aba"],["abab","bbabba","b","a"],["ababbbabba","b","a"]]
 # s = "baba" # [["b","a","b","a"],["b","aba"],["bab","a"]]
 # Wrong answer: [["b","a","b","a"],["b","aba"],["baba"],["bab","a"]]
 
@@ -36,7 +36,7 @@ s = "abbab" # [["a","b","b","a","b"],["a","b","bab"],["a","bb","a","b"],["abba",
 from typing import List
 import collections
 class Solution:
-    def partition(self, s: str) -> List[List[str]]:        
+    def partition(self, s: str) -> List[List[str]]:
         def dfs(memo, chars):
             if len(chars) == 1:
                 # memo[chars] = chars
@@ -97,7 +97,7 @@ class Solution:
 
 # Without all the hashed out code lines
 class Solution:
-    def partition(self, s: str) -> List[List[str]]:        
+    def partition(self, s: str) -> List[List[str]]:
         def dfs(memo, chars):
             if len(chars) == 1:
                 return [[chars]]
@@ -133,6 +133,70 @@ class Solution:
 
 # Runtime: 972 ms, faster than 5.04% of Python3 online submissions for Palindrome Partitioning.
 # Memory Usage: 34.3 MB, less than 7.20% of Python3 online submissions for Palindrome Partitioning.
+
+# s = abbab # [["a","b","b","a","b"],["a","b","bab"],["a","bb","a","b"],["abba","b"]]
+# s = "abbccbbab"
+# s = "abbccb"
+s = "aaaaa"
+
+
+# Backtracking
+# Recursively
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def isValid(begin, end):
+            if end == begin:
+                return True
+            if end - begin == 1 or end - begin == 2:
+                if s[begin] == s[end]:
+                    return True
+            return False
+        
+        def foundSolution(idx):
+            if idx == len(s):
+                return True
+            return False
+        
+        def placing(memo, begin, end):
+            memo.append(s[begin:end+1])
+
+        def removing(memo):
+            memo.pop()
+        
+        def backtrack(start, memo):
+            if foundSolution(start):
+                candidate = memo.copy()
+                self.res.append(candidate)
+                return
+            # looking forward
+            for i in range(start, len(s)):
+                if isValid(start, i):
+                    placing(memo, start, i)
+                    backtrack(i+1, memo)
+                    # to cover the case: "bbccbb"
+                    # if memo[]
+                    removing(memo)
+            # i, steps = start, 1
+            # i = start
+            # while i < len(s):
+            #     if isValid(start, i):
+            #         placing(memo, start, i)
+            #         # steps = len(memo[-1])
+            #         backtrack(i+1, memo)
+            #         i += 1
+            #         # prePalLen = len(memo[-1])
+            #         removing(memo)
+                # else:
+                #     break
+            # return prePalLen
+
+        self.res = []
+        memo = []
+        backtrack(0, memo)
+        return self.res
+
+
+
 
 
 solution = Solution()
