e65c09729ce964496c63ce9d5d04faba327919aa
Gang Liu <lucliu@hotmail.com>
2021-09-16 10:43:54 -0700

    Update 978-Longest Turbulent Subarray.py

--- 978-Longest Turbulent Subarray.py
+++ 978-Longest Turbulent Subarray.py
@@ -16,10 +16,10 @@ Output: 1
 """
 
 
-# arr = [9,9,4,2,10,7,8,8,1,9,9]
+arr = [9,4,2,10,7,8,8,1,9]
 # arr = [4,8,12,16]
 # arr = [100]
-arr = [0,8,45,88,48,68,28,55,17,24] # Expected: 8
+# arr = [0,8,45,88,48,68,28,55,17,24] # Expected: 8
 
 
 
@@ -58,6 +58,27 @@ class Solution:
 
 # Runtime: 460 ms, faster than 92.17% of Python3 online submissions for Longest Turbulent Subarray.
 # Memory Usage: 18.4 MB, less than 87.99% of Python3 online submissions for Longest Turbulent Subarray.
+
+
+
+# DP iteratively
+# Refer to the LeetCode post:
+# https://leetcode.com/problems/longest-turbulent-subarray/discuss/222511/DP-Thinking-Process-(Java)
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        dp = [[0 for _ in range(2)] for _ in range(len(arr))]
+        res = 0
+        for i in range(1, len(arr)):
+            if arr[i] > arr[i-1]:
+                dp[i][1] = dp[i-1][0] + 1
+                res = max(res, dp[i][1])
+            elif arr[i] < arr[i-1]:
+                dp[i][0] = dp[i-1][1] + 1
+                res = max(res, dp[i][0])
+        return res + 1
+
+# Runtime: 1031 ms, faster than 5.22% of Python3 online submissions for Longest Turbulent Subarray.
+# Memory Usage: 19.8 MB, less than 8.62% of Python3 online submissions for Longest Turbulent Subarray.
 
 
 
