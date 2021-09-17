057b906f47a15a4264ea5425559c5661ffc0c82a
Gang Liu <lucliu@hotmail.com>
2021-09-17 09:18:41 -0700

    Update 1870-Minimum Speed to Arrive on Time.py

--- 1870-Minimum Speed to Arrive on Time.py
+++ 1870-Minimum Speed to Arrive on Time.py
@@ -27,10 +27,10 @@ Explanation: It is impossible because the earliest the third train can depart is
 """
 
 
-# dist, hour = [1,3,2], 6
+dist, hour = [1,3,2], 6
 # dist, hour = [1,3,2], 2.7
 # dist, hour = [1,3,2], 1.9
-dist, hour = [1,1,100000], 2.01
+# dist, hour = [1,1,100000], 2.01 # Expected: 10000000
 
 
 # 6 / 53 test cases passed.
@@ -46,6 +46,58 @@ class Solution:
 
 
 
+
+# Sep/16/2021 attemp
+import math
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        def ArrTime(speed):
+            total = 0
+            for i in range(len(dist)-1):
+                tmp = math.ceil(dist[i] / speed)
+                total += tmp
+            return total + (dist[-1] / speed)
+
+        fastest = 10000000
+        mid = (fastest + 1) // 2
+        while mid in range(1, fastest+1):
+            travel = ArrTime(mid)
+            if travel == hour:
+                return mid
+            elif travel > hour: # need a faster speed
+                mid = (fastest + mid + 1) // 2
+            else: # need a slower speed
+                fastest = mid
+                mid = (fastest + 1) // 2
+                
+        return -1
+
+
+
+# Sep/17/2021 attemp
+# Real Binary Search way
+# Time complexity: O(nlogR), where R is the possible speed range
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        def ArrTime(speed):
+            total = 0
+            for i in range(len(dist)-1):
+                tmp = math.ceil(dist[i] / speed)
+                total += tmp
+            return total + (dist[-1] / speed)
+
+        slowest, fastest = 1, 10000000 + 1
+        while slowest < fastest:
+            mid = slowest + (fastest - slowest) // 2
+            if ArrTime(mid) > hour: # need a faster speed
+                slowest = mid + 1
+            else: # need a slower speed
+                fastest = mid
+        return -1 if slowest == 10000000 + 1 else slowest
+
+
+# Runtime: 4196 ms, faster than 41.06% of Python3 online submissions for Minimum Speed to Arrive on Time.
+# Memory Usage: 28.9 MB, less than 6.80% of Python3 online submissions for Minimum Speed to Arrive on Time.
 
 
 solution = Solution()
