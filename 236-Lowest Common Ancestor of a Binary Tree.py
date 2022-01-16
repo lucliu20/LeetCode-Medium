cf2cee05dc501db90b3253eaf319062d2e11356e
Gang Liu <lucliu@hotmail.com>
2022-01-16 13:10:17 -0800

    Update 236-Lowest Common Ancestor of a Binary Tree.py

--- 236-Lowest Common Ancestor of a Binary Tree.py
+++ 236-Lowest Common Ancestor of a Binary Tree.py
@@ -25,17 +25,17 @@ class TreeNode:
         self.right = None
 
 
-# root = TreeNode(3)
-# root.left = TreeNode(5)
-# root.right = TreeNode(1)
-# root.left.left = TreeNode(6)
-# root.left.right = TreeNode(2)
-# root.right.left = TreeNode(0)
-# root.right.right = TreeNode(8)
-# root.left.right.left = TreeNode(7)
-# root.left.right.right = TreeNode(4)
+root = TreeNode(3)
+root.left = TreeNode(5)
+root.right = TreeNode(1)
+root.left.left = TreeNode(6)
+root.left.right = TreeNode(2)
+root.right.left = TreeNode(0)
+root.right.right = TreeNode(8)
+root.left.right.left = TreeNode(7)
+root.left.right.right = TreeNode(4)
 # p, q = root.left, root.right
-# p, q = root.left, root.left.right.right
+p, q = root.left, root.left.right.right
 
 
 # root = TreeNode(1)
@@ -46,13 +46,13 @@ class TreeNode:
 # [-1,0,3,-2,4,null,null,8]
 # 8
 # 4
-root = TreeNode(-1)
-root.left = TreeNode(0)
-root.right = TreeNode(3)
-root.left.left = TreeNode(-2)
-root.left.right = TreeNode(4)
-root.left.left.left = TreeNode(8)
-p, q = root.left.left.left, root.left.right
+# root = TreeNode(-1)
+# root.left = TreeNode(0)
+# root.right = TreeNode(3)
+# root.left.left = TreeNode(-2)
+# root.left.right = TreeNode(4)
+# root.left.left.left = TreeNode(8)
+# p, q = root.left.left.left, root.left.right
 
 
 # Recursively
@@ -108,6 +108,23 @@ class Solution:
 # Follow up
 # Refer to the post: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/discuss/158060/Python-DFS-tm,
 # for a simpler strategy.
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root: return None
+        if p == root or q == root:
+            return root
+        left = self.lowestCommonAncestor(root.left, p , q)
+        right = self.lowestCommonAncestor(root.right, p , q)
+        
+        if left and right:
+            return root
+        if not left:
+            return right
+        if not right:
+            return left
+
 
 solution = Solution()
 print(solution.lowestCommonAncestor(root, p, q))
